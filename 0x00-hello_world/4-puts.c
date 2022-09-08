20253ff52352c4b2831bd733a157dd7c31f40a4f
Rowland Oka <rowlandoka@gmail.com>
2022-09-08 15:59:17 -0700

    fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - print to the standard output 'Programming is a multilingual puzzle'
+ * main - Entry point
  *
- * Use Puts
- * Return : return 0
+ * Return: always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a  multilingual puzzle");
 	return (0);
-}	
+}
