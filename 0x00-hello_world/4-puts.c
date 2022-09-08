e485401b67a9dfa6923e195337d82722265ca706
Rowland Oka <rowlandoka@gmail.com>
2022-09-08 15:52:30 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  * Use Puts
  * Return : return 0
  */
- int main(void)
- {
-         puts("\"Programming is like building a multilingual puzzle");
-         return (0);
- }	 
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
