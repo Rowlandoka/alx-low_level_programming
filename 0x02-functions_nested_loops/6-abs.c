bf0d0bddbe51aabed1a89414adddb720e5061753
Rowland Oka <rowlandoka@gmail.com>
2022-09-13 15:25:56 -0700

    completed task 3-6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Compute the absolute value of an integer
+ * @n: Value to be computed
+ *
+ * Return: The absolute value of the integer
+ */
+int _abs(int)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (n * -1);
+}
