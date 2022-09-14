72123af8b8282d414510d350b9891e084bec2fa6
Rowland Oka <rowlandoka@gmail.com>
2022-09-14 16:25:28 -0700

    resolve task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,14 @@
 
 /**
  * main - tests if integer is positive or negative
- *
  * Return: 0
  */
 int main(void)
 {
-	int i;
+int i;
 
-	i = 0;
-	positive_or_negative(i);
-	return (0);
+i = 0;
+positive_or_negative(i);
+
+return (0);
 }
