683e2bb0d3f9771b9ba2f5f2e00343b248d9d08b
Rowland Oka <rowlandoka@gmail.com>
2022-10-03 18:58:32 +0100

    fixed errors

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  *main - adds two positive numbers
  *@argc: number of arguments passed
@@ -8,23 +9,23 @@
  */
 int main(int argc, char *argv[])
 {
-    int a = 0, i, j;
+int a = 0, i, j;
 
-    for (i = 1; i < argc; i++)
-    {
-        for (j = 0; argv[i][j]; j++)
-        {
-            if (isdigit(argv[i][j] == 0))
-            {
-                puts("Error");
-                return (1);
-            }
-        }
-    }
-    for (i = 1; i < argc; i++)
-    {
-        a += atoi(argv[i]);
-    }
-    printf("%d\n", a);
-    return (0);
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j]; j++)
+{
+if (isdigit(argv[i][j] == 0))
+{
+puts("Error");
+return (1);
+}
+}
+}
+for (i = 1; i < argc; i++)
+{
+a += atoi(argv[i]);
+}
+printf("%d\n", a);
+return (0);
 }
