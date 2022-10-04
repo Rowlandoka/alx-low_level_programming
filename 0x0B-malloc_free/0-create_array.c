aa8c41dffcdccbf05e9b180a139f7273fb4196be
Rowland Oka <rowlandoka@gmail.com>
2022-10-04 18:23:06 +0100

    fixed errors

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 	if (size != 0)
 	{
 		array = (char *)malloc(size sizeof(char));
-		if (size != NULL)
+		if (array != NULL)
 		{
 			for (i = 0; i < size; i++)
 				array[i] = c;
