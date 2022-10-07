12ea03ff7111e7642983a49e7e0772d7bd5f2b24
Rowland Oka <rowlandoka@gmail.com>
2022-10-07 01:30:57 +0100

    resolved task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,32 +9,38 @@
 */
 char *string_concat(char *s1, char *s2, unsigned int n)
 {
-	char *s;
-	unsigned int i, j, k;
+	char *p;
+	unsigned int size1 = 0, size2 = 0, i;
 
 	if (s1 == NULL)
-		i = 0;
-	else
-	{
-		for (i = 0; s1[i]; i++)
-			;
-	}
+		s1 = "";
 	if (s2 == NULL)
-		j = 0;
-	else
+		s2 = "";
+
+	while (s1[size1] != '\0')
 	{
-		for (j = 0; s2[j]; j++)
-			;
+		s1++;
 	}
-	if (j > n)
-		j = n;
-	s = malloc(sizeof(char) * (i + j + 1));
-	if (s == NULL)
+
+	while (s2[size2] != '\0')
+	{
+		s2++;
+	}
+	if (n > size2)
+		n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
+	if (p == NULL)
 		return (NULL);
-	for (k = 0; k < i; k++)
-		s[k] = s1[k];
-	for (k = 0; k < j; k++)
-		s[k + j] = s2[k];
-	s[i + j] = '\0';
-	return (s);
+
+	for (i = 0; i < size1; i++)
+	{
+		p[i] = s1[i];
+	}
+
+	for (; i < (size1 + n); i++)
+	{
+		p[i] = s2[i - size1];
+	}
+	p[i] = '\0';
+	return (p);
 }
