6882e45ae91354b6023931b4bc31a49a36c53431
Rowland Oka <rowlandoka@gmail.com>
2022-10-13 03:17:06 +0100

    resolved errors

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
-#include "function_pointers.h"
-#include <stdio.h>
 #include <stdlib.h>
-
+#include <stdio.h>
+#include "function_pointers.h"
 /**
- * print_name - function that prints name
- * @name: person name
- *@f: pointer function passed as parameter
- * Return: void
+ * print_name - print the name
+ * @name: name to print
+ * @f: function to use
  */
 void print_name(char *name, void (*f)(char *))
 {
-if (name != NULL && f != NULL)
-f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
+
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,15 +1,13 @@
-#include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "function_pointers.h"
 /**
-*int_index - function that searches integer
-*@array: array pointer to search
-*@size: size of the array
-*@cmp: pointer function parameter
-*
-*Return: return -1 if fail, else otherwise
-*/
+ * int_index - indexes aray i think
+ * @array: array to go through
+ * @size: size of array
+ * @cmp: function to do something with size
+ * Return: int value
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
