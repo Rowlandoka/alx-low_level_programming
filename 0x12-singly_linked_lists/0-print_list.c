e7a96d48d342b2bae293189bbf82fc588565a640
Rowland Oka <rowlandoka@gmail.com>
2022-10-21 14:54:56 +0100

    fixed betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,16 +1,22 @@
 #include "lists.h"
+/**
+ * print_list - prints the elements of a list
+ *@h:elements to prints
+ *Return: number of nodes
+ */
 
 size_t print_list(const list_t *h)
 {
-    const list_t *temp;
-    unsigned int i = 0;
+const list_t *temp;
+unsigned int i = 0;
 
-    temp = h;
-    while (temp != NULL)
-    {
-        printf("[%u] %s\n", temp->len, temp->str);
-        temp = temp->next;
-        i++;
-    }
-    return (i);
+temp = h;
+while (temp != NULL)
+{
+printf("[%u] %s\n", temp->len, temp->str);
+temp = temp->next;
+i++;
 }
+return (i);
+}
+
