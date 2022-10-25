03408e06b8ef48ec30e75c9fb847e863f0b242fe
Rowland Oka <rowlandoka@gmail.com>
2022-10-25 19:21:54 +0100

    fixed

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,17 @@
 
 void free_listint2(listint_t **head)
 {
-listint_t *current;
+listint_t *current, *temp;
 
+if (head == NULL)
+return (NULL);
+
+current = *head;
 while (current != NULL)
 {
-current = *head;
-*head = NULL;
-free(*head);
+temp = current;
+current = current->next;
+free(temp);
 }
+*head = NULL;
 }
