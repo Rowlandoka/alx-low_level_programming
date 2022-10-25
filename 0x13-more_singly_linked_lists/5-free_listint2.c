8954e093b082d8aa8aeb7a3f38c8b5ab59f1296a
Rowland Oka <rowlandoka@gmail.com>
2022-10-25 19:16:06 +0100

    error fixed

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 
 /**
- *free_listint2 - free node 
+ *free_listint2 - free node
  *@head: head pointer
  *Return: void
  */
 
 void free_listint2(listint_t **head)
 {
-    listint_t *current;
+listint_t *current;
 
-    while (current != NULL)
-    {
-         current = *head;
-         *head = NULL;
-         free(*head);
-    }
+while (current != NULL)
+{
+current = *head;
+*head = NULL;
+free(*head);
+}
 }
