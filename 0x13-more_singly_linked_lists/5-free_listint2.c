fe68936dc03d4516b59f4aaf5e1667f23ca36128
Rowland Oka <rowlandoka@gmail.com>
2022-10-25 19:24:47 +0100

    fixed betty

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,18 +1,14 @@
 #include "lists.h"
-
 /**
- *free_listint2 - free node
- *@head: head pointer
- *Return: void
- */
-
+  * free_listint2 - Frees a list and sets its head to NULL
+  * @head: double pointer to head of list
+  */
 void free_listint2(listint_t **head)
 {
-listint_t *current, *temp;
+listint_t *temp, *current;
 
 if (head == NULL)
-return (NULL);
-
+return;
 current = *head;
 while (current != NULL)
 {
