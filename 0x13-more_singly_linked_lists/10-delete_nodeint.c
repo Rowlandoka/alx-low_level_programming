72f39641f9a63acf85e4c4f2a5b2fa99da2ac4c8
Rowland Oka <rowlandoka@gmail.com>
2022-10-26 20:06:57 +0100

    fixed error

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,10 +10,10 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-listint_t *temp, *prev;
 unsigned int i;
+listint_t *temp, *prev;
 
-if (!head || *head)
+if (!head || !*head)
 return (-1);
 temp = *head;
 if (index == 0)
