293efbb797f77deed8823574c4b88f578d50d3de
Rowland Oka <rowlandoka@gmail.com>
2022-10-28 17:36:32 +0100

    fixed errors

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,32 +1,27 @@
 #include "main.h"
+
 /**
- * binary_to_uint - Entry Point
- * @b: const char
- * Return: 0
+ *binary_to_uint - converts a binary number to an unsigned int
+ *@b: string of binary
+ *
+ *Return: 0
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int res = 0;
-	int base = 1, i = 0;
+int i;
+unsigned int uint = 0;
 
-	if (b == NULL)
-		return (0);
-
-	while (b[i + 1])
-	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-		i++;
-	}
-
-	while (i >= 0)
-	{
-		res += ((b[i] - '0') * base);
-		base *= 2;
-		i--;
-	}
-
-
-	return (res);
-
+if (!b)
+return (0);
+for (i = 0; b[i] != '\0'; i++)
+{
+if (b[i] != '0' && b[i] != '1')
+return (0);
+else
+uint <<= 1;
+if (b[i] == '1')
+uint += 1;
+}
+return (uint);
 }
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,33 +1,48 @@
 #include "main.h"
+
 /**
- * print_binary - Entry Point
- * @n: dec input
- * Return: 0
+ *_pow - calculates (base ^ power)
+ *@base: bse of the exponent
+ *@power: powre of the exponent
+ *
+ *Return: value of (base ^ power)
  */
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+unsigned long int num;
+unsigned int i;
+
+num = 1;
+for (i = 1; i <= power; i++)
+num *= base;
+return (num);
+}
+
+/**
+ *print_binary - prints the binary representation of a number
+ *@n: binary number
+ *
+ *Return 0
+ */
+
 void print_binary(unsigned long int n)
 {
-	int i = 0, count, k, temp;
+unsigned long int divisor, checker;
+char flag = 0;
 
-	if (n == 0)
-	{
-		printf("0");
-		return;
-	}
-
-	temp = n;
-
-	while (temp != 0)
-	{
-		i++;
-		temp = temp >> 1;
-	}
-
-	for (count = i - 1; count >= 0; count--)
-	{
-		k = n >> count;
-		if (k & 1)
-			printf("1");
-		else
-			printf("0");
-	}
+divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+while (divisor != 0)
+{
+checker = n & divisor;
+if (checker == divisor)
+{
+flag = 1;
+_putchar('1');
+}
+else if (flag == 1 || divisor == 1)
+{
+_putchar ('0');
+}
+divisor >>= 1;
+}
 }
