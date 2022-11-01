cec27f256d72ccc3a0c41ad4cf1a6825b4e4be59
Rowland Oka <rowlandoka@gmail.com>
2022-11-01 01:33:39 +0100

    error fixed

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,8 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
-#include<sys/types.h>
-#include<sys/stat.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h> 
 #include <stdio.h>
 #include <unistd.h>
