5f4bd64e2939f604d34eeb5d6bd2e9cd2ecc9f2e
Rowland Oka <rowlandoka@gmail.com>
2022-12-08 18:34:42 +0100

    setup project and created task 0ne

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print all elements of dlistint
+ * @h: pointer to the start of the list
+ *
+ * Return: number of nodes in the list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+    int count = 0;
+
+    while (h != NULL)
+    {
+        if (h->n == 0)
+            printf("[0] (nil)\n");
+        else
+            printf("%d\n", h->n);
+        h = h->next;
+        count++;
+}
+return (count);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+#Working of Doubly linked list - Data Structures and Algorithms
